768904b7856fbce7e18c0a780b6558107facb5cd
Evan Chen <evan@Evans-MacBook-Pro.local>
2018-03-27 00:18:32 -0700

    added basic stroke

--- src/com/chewychiyu/paint/PaintPanel.java
+++ src/com/chewychiyu/paint/PaintPanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -64,6 +65,7 @@ public class PaintPanel extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		stroke(g2d);
 		
 	}
